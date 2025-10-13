8797213e134ea047baddbc01b803cc81cce5a6e6
lawrencemacababayao <lawrencemacababayao2>
2025-10-13 17:13:19 +0800

    iems

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
 
-  base: process.env.VITE_BASE_PATH || "/sangguniangkabataan_iems",
+  base: "/",
   plugins: [
     react(),
     tailwindcss(),
