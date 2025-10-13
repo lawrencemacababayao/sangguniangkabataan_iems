e9c5a5cc09ea0374e98d3f1459018107c9b564cf
lawrencemacababayao <lawrencemacababayao2>
2025-10-14 02:05:14 +0800

    iems

--- src/pages/Startt.jsx
+++ src/pages/Startt.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import bgImage from "../assets/ChatGPT Image Oct 11, 2025, 02_38_22 PM.png";
 import logoImage from "../assets/skkkk.png"
 import { useNavigate } from "react-router-dom";  //hook for navigating between pages 
+import "../media/start.css"
 
 const Startt = () => {
 
@@ -20,14 +21,14 @@ const handleYouthClick = () => {
       {/* Optional overlay (for dark filter) */}
 
       {/* content layer */}
-      <div className="relative z-10 flex items-center justify-center h-full pr-10 pl-10">
-        <form className='bg-[#F3F1FF]/70 pt-5 mt-0 rounded-2xl shadow-xl w-full max-w-md h-3/4 flex flex-col items-center '>
+      <div className="relative z-10 flex items-center justify-center h-full pr-5 pl-5">
+        <form className='form bg-[#F3F1FF]/70 pt-5 mt-0 rounded-2xl shadow-xl w-full max-w-md h-3/4 flex flex-col items-center '>
           <div className='w-3/4 min-h-1/4 flex justify-center'><img src={logoImage} alt="" className=' w-1/2 object-cover'/></div>
-          <h3 className='text-2xl font-medium'>SANGGUNIANG</h3>
-          <h1 className='text-5xl font-bold'>KABATAAN</h1>
+          <h3 className='starttext1 text-2xl font-medium'>SANGGUNIANG</h3>
+          <h1 className='starttext2 text-5xl font-bold'>KABATAAN</h1>
 
-          <button onClick={handleYouthClick} className='bg-[#0049B1] p-1 w-1/2 mt-15 rounded-xl text-white h-12 cursor-pointer text-xl flex justify-center items-center gap-2'><span className="material-icons-outlined  text-50 text-white font-thin">person</span>YOUTH</button>
-          <button className='bg-[#0049B1] p-1 mt-10 w-1/2 rounded-xl text-white h-12 cursor-pointer text-xl flex justify-center items-center gap-2'><span className="material-icons-outlined  text-50 text-white font-thin">manage_accounts</span>ADMIN</button>
+          <button onClick={handleYouthClick} className='btn1 bg-[#0049B1] p-1 w-1/2 mt-15 rounded-xl text-white h-12 cursor-pointer text-xl flex justify-center items-center gap-2'><span className="material-icons-outlined  text-50 text-white font-thin">person</span>YOUTH</button>
+          <button className='btn2 bg-[#0049B1] p-1 mt-10 w-1/2 rounded-xl text-white h-12 cursor-pointer text-xl flex justify-center items-center gap-2'><span className="material-icons-outlined  text-50 text-white font-thin">manage_accounts</span>ADMIN</button>
         </form>
       </div>
     </div>
